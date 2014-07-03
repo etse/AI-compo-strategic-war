f49c836337315cf6da81fd6139aaae1c588e03c6
Steffen Hageland <etse88@gmail.com>
2014-07-03 20:44:39 +0200

    added a smartClient, as an alternative to the dumb testClient.

--- testClient.py
+++ testClient.py
@@ -141,16 +141,10 @@ class GameAI:
         command = {"mode": random.choice(["standard", "harvester", "soldier"]), "moves": []}
         for unit in filter(lambda u: u.owner == self.my_id, self.board.units):
             x, y = unit.position
-            legal_directions = []
-            for nx, ny, direction in [(x, y-1, "north"), (x, y+1, "south"), (x-1, y, "west"), (x+1, y, "east")]:
-                rx, ry = self.board.get_real_position(nx, ny)
-                if not self.board.any_units_on_position((rx, ry)) and not self.board[rx][ry].isWall:
-                    legal_directions.append(direction)
-
-            if legal_directions:
-                direction = random.choice(legal_directions)
-                command["moves"].append([x, y, direction])
-                self.board.move_unit(x, y, direction)
+            legal_directions = ["north", "south", "west", "east"]
+            direction = random.choice(legal_directions)
+            command["moves"].append([x, y, direction])
+            self.board.move_unit(x, y, direction)
         self.send_command(command)
 
 
